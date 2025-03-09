8ff52730dc615295ad8a2b9316e4b2ed4b599ba3
AnhMinh <truonghoanganhminh2000@gmail.com>
2025-03-09 21:53:32 +0700

    9/3

---
+++ src/components/sections/hero/hero.left.tsx
@@ -0,0 +1,81 @@
+import Typewriter from "typewriter-effect";
+import SocialMedia from "../parts/social.media.js";
+import { useTranslation } from "react-i18next";
+import "../hero/hero.scss";
+import ResizeButton from "../parts/resize.button.tsx";
+import { APP_DATA } from "@/helpers/data.js";
+import { MdFileDownload } from "react-icons/md";
+import { AiFillFire } from "react-icons/ai";
+
+interface IProps {
+  scrollToExperienceSection: () => void;
+}
+
+const HeroLeft = (props: IProps) => {
+  const { t } = useTranslation();
+
+  const openInNewTab = (url: string): void => {
+    const newWindow = window.open(url, "_blank", "noopener,noreferrer");
+    if (newWindow) newWindow.opener = null;
+  };
+
+  const handleDownLoadCV = () => {
+    openInNewTab("https://www.youtube.com/watch?v=BDwnnKp35B4");
+  };
+
+  return (
+    <div className="hero-left">
+      <h3>
+        Hi There!{" "}
+        <span className="wave" role="img" aria-labelledby="wave">
+          👋🏻
+        </span>
+      </h3>
+      <h3 style={{ paddingTop: 10, paddingBottom: 5 }}>
+        I'M &nbsp;
+        <strong className="brand-red">{t("appHeader.brand")}</strong>
+      </h3>
+      <Typewriter
+        options={{
+          strings: [
+            "Software Developer",
+            "Freelancer",
+            "MERN Stack Developer",
+            "Open Source Contributor",
+          ],
+          autoStart: true,
+          loop: true,
+          deleteSpeed: 50,
+          wrapperClassName: "brand-green",
+        }}
+      />
+      <div className="mt-md-6 mt-3 mb-md-5 mb-2">
+        <SocialMedia
+          youtube={APP_DATA.YOUTUBE_URL}
+          facebook={APP_DATA.FACEBOOK_URL}
+          tiktok={APP_DATA.TIKTOK_URL}
+          udemy={APP_DATA.UDEMY_URL}
+        />
+      </div>
+      <div className="d-md-flex d-none gap-4">
+        <ResizeButton
+          onClick={props.scrollToExperienceSection}
+          btnText={t("heroSection.exp")}
+          btnIcons={<AiFillFire style={{ color: "orange" }} />}
+          btnStyle={{
+            background: "unset",
+            border: "1px solid var(--border-hero-right)",
+            color: "var(--text-white-1)",
+          }}
+        />
+        <ResizeButton
+          onClick={handleDownLoadCV}
+          btnText={t("heroSection.cv")}
+          btnIcons={<MdFileDownload />}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default HeroLeft;
